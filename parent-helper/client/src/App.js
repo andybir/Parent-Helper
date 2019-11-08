821b8113b13e5e34a94a9865ed3101d12dbd72df
Andy Birosak <abirosak@gmail.com>
2019-11-08 10:27:03 -0500

    routing built for AllTopics cmpnt

--- parent-helper/client/src/App.js
+++ parent-helper/client/src/App.js
@@ -1,13 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios'
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h2>Parent Helper</h2>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      topics: [],
+      currentTopic: {},
+      topicLoaded: false
+    }
+  }
+
+  getAllTopics = () => {
+    axios('http://localhost:3000/topics').then(jsonRes => {
+      this.setState({
+        topics: jsonRes.data.topics,
+        topicsLoaded: true
+      })
+    })
+  }
+
+  setTopic = (topic) => {
+    this.setState({
+      currentTopic: topic
+    })
+  }
+
+
+  render () {
+    return (
+      <Router>
+        <div className="App">
+          <Link exact='true' to='/'>
+            Parent Helper
+          </Link>
+          <Switch>
+            <Route exact path='/' render={() => (
+              <AllTopics
+              getAllTopics={this.getAllTopics}
+              topics={this.setState.topics}
+              topicsLoaded={this.state.topicsLoaded}
+              setTopic={this.setTopic} />
+            )} />
+          </Switch>
+        </div>
+      </Router>
+    )
+  }
 }
 
 export default App;
