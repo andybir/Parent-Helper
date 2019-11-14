c86071c3ea2a6bef89e24208b3dc75889de3f4fb
Andy Birosak <abirosak@gmail.com>
2019-11-14 09:51:43 -0500

    imported jjByrne jpeg

--- parent-helper/client/src/components/Home.js
+++ parent-helper/client/src/components/Home.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import jjByrne from '../images/jj-byrne-pg.jpg'
 
 
 const Home = () => {
 
     return (
-        <div>
+        <div className='home'>
             <h2><Link to='/topics'>Browse Topics</Link></h2>
             <h2><Link to='/new-post'>Create Post</Link></h2>
+            <img src={jjByrne} alt='jj-byrne-pg' />
         </div>
     )
 
