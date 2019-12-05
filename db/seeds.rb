8f56cb954f13a221d4746795d6727b43448eba6f
Andy Birosak <abirosak@gmail.com>
2019-12-05 16:36:31 -0500

    commented out seed data to eliminate redundancies

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,19 +11,19 @@ User.create(username: "birdieb", email: "birdieb@gmail.com", password: "password
 
 puts "#{User.count} users created!"
 
-Post.create(title: "Fun party", content: "Come to my fun part on Friday. It's going to be a blast!", subject: "Events", user_id: 1, topic_id: 2 )
-Post.create(title: "Happy hour", content: "let's meet up at the bar to have some happy hour and celebrate the weekend!!!", subject: "Events", user_id: 1, topic_id: 1)
-Post.create(title: "plumber", content: "Anyone need a recommendation for a plumber?  I've got a great one named Luigi.", subject: "Household", user_id: 1, topic_id: 3)
+# Post.create(title: "Fun party", content: "Come to my fun part on Friday. It's going to be a blast!", subject: "Events", user_id: 1, topic_id: 2 )
+# Post.create(title: "Happy hour", content: "let's meet up at the bar to have some happy hour and celebrate the weekend!!!", subject: "Events", user_id: 1, topic_id: 1)
+# Post.create(title: "plumber", content: "Anyone need a recommendation for a plumber?  I've got a great one named Luigi.", subject: "Household", user_id: 1, topic_id: 3)
 
 puts "#{Post.count} posts created!"
 
-Topic.create(subject: "General Advice", description: "Ask questions about child behavior. Or topics like pregnancy and relationship advice")
-Topic.create(subject: "Events", description: "Post here for topics relating to events going on in your neighborhood.")
-Topic.create(subject: "Home Improvement", description: "Post here to recommend or not recommend a handymen.")
+# Topic.create(subject: "General Advice", description: "Ask questions about child behavior. Or topics like pregnancy and relationship advice")
+# Topic.create(subject: "Events", description: "Post here for topics relating to events going on in your neighborhood.")
+# Topic.create(subject: "Home Improvement", description: "Post here to recommend or not recommend a handymen.")
 
 
 puts "#{Topic.count} topics created"
 
-Comment.create(title: "I agree with you", content: "I definitely agree with your post. It's the best post I've ever read. I wish I had written it myself. I'm soooo jealous.", post_id: 1)
+# Comment.create(title: "I agree with you", content: "I definitely agree with your post. It's the best post I've ever read. I wish I had written it myself. I'm soooo jealous.", post_id: 1)
 
 puts "#{Comment.count} comments created"
